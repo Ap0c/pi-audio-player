f8842d0f3e8342f55fcb35a46c6acd0e7eb82df5
Ap0c <Ap0c@users.noreply.github.com>
2016-04-08 22:39:30 +0100

    Added istanbul as dev dep for test coverage. Added tests for queue get and clear methods. Switched queue to return Queue power constructor.

--- test/queue-test.js
+++ test/queue-test.js
@@ -12,11 +12,22 @@ describe('Queue', function () {
 
 	let queue = null;
 
-	describe('#append', function () {
+	beforeEach(function () {
+		queue = Queue();
+	});
 
-		beforeEach(function () {
-			queue = Queue();
+	describe('#get', function () {
+
+		it('should retrieve the upcoming items in the queue', function () {
+
+			let upcoming = queue.get();
+			expect(upcoming).to.eql([]);
+
 		});
+
+	});
+
+	describe('#append', function () {
 
 		it('should add an item to the queue', function () {
 
@@ -35,6 +46,22 @@ describe('Queue', function () {
 
 			let items = queue.get();
 			expect(items).to.eql(toAppend);
+
+		});
+
+	});
+
+	describe('#clear', function () {
+
+		it('should clear the items in the queue', function () {
+
+			let toAppend = [{ a: 1 }, { b: 2 }];
+			queue.append(toAppend);
+
+			queue.clear();
+
+			let items = queue.get();
+			expect(items).to.eql([]);
 
 		});
 
