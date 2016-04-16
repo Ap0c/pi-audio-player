55a952dcdee9f5949de19f7f53abc9dd0bc01df9
Ap0c <Ap0c@users.noreply.github.com>
2016-04-16 15:43:52 +0100

    Add testing module for socket interface.

---
+++ test/socket-test.js
@@ -0,0 +1,21 @@
+'use strict';
+
+// ----- Requires ----- //
+
+let expect = require('chai').expect;
+let request = require('supertest');
+let io = require('socket.io-client');
+let server = require('../server');
+
+
+// ----- Tests ----- //
+
+describe('Socket', function () {
+
+	describe('playback', function () {
+
+
+
+	});
+
+});
